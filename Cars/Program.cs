c72f40edafbd95b97231bf1e6a866cbb65641174
Theo <Theo@gmail.com>
2023-11-19 14:27:26 +0200

    Add project files.

---
+++ Cars/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            car Ford = new car();
+            Ford.name = "Mustang";
+            Ford.year = 2021;
+            Ford.color = "white";
+            Ford.maxSpeed = 100;
+            Console.WriteLine(Ford.name);
+            Console.WriteLine(Ford.year);
+            Console.WriteLine(Ford.color);
+            Ford.fullThrottle();
+            car Opel = new car();
+            Opel.name = "Astra";
+            Opel.year = 2020;
+            Opel.color = "blue";
+            Opel.maxSpeed = 120;
+            Console.WriteLine(Opel.name);
+            Console.WriteLine(Opel.year);
+            Console.WriteLine(Opel.color);
+            Opel.fullThrottle();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Cars/car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{
+    public class car
+    {
+        public string color;
+        public string name;
+        public int maxSpeed;
+        public int year;
+        public void fullThrottle()
+        {
+            Console.WriteLine(name + "is going at the seed of" + maxSpeed);
+        }
+    }
+}
